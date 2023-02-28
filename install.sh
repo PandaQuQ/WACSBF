26cadccc7c5ac4b6b7198ad4dcebb7dd262611d8
panda <mabinsoft@vip.qq.com>
2023-02-28 20:38:04 +0800

    添加功能:自动建立alias, 自动创建目录, 安装脚本, 开机自动运行, 写入bspwm config

---
+++ install.sh
@@ -0,0 +1,27 @@
+function makeConfig(){
+	shell_type="$(echo $SHELL | grep -E -o '[[:alpha:]]{1,2}sh')"
+	bashrc_path=~/.bashrc
+	zshrc_path=~/.zshrc
+	bspwmrc_path=~/.config/bspwm/bspwmrc
+	mkdir -p ~/.config/WACSBF/scripts
+	mkdir -p ~/.config/WACSBF/cache
+	mkdir -p ~/.config/WACSBF/downloaded_wallpapers
+	if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
+		echo "PATH="\$HOME/.config/WACSBF/scripts:\$PATH"" >> $zshrc_path
+		echo "alias wacsbf='sh \$HOME/.config/WACSBF/scripts/WACSBF'" >> $zshrc_path
+	elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
+		echo "PATH="\$HOME/.config/WACSBF/scripts:\$PATH"" >> $zshrc_path
+		echo "alias wacsbf='sh \$HOME/.config/WACSBF/scripts/WACSBF'" >> $zshrc_path
+	else
+	    echo "不支持的shell类型"
+	    exit
+	fi
+	if [[ -f $bspwmrc_path ]]; then
+		echo "wacsbf" >> $bspwmrc_path
+	fi
+
+}
+makeConfig
+cp ./WACSBF $HOME/.config/WACSBF/scripts/WACSBF
+
+echo "finished"
