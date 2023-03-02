df4835be5e88316dbb1f496ed2481575b60a07cc
panda <mabinsoft@vip.qq.com>
2023-03-02 18:29:06 +0800

    optimize the process of installation

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 function makeConfig(){
 	shell_type="$(echo $SHELL | grep -E -o '[[:alpha:]]{1,2}sh')"
 	bashrc_path=~/.bashrc
 	zshrc_path=~/.zshrc
 	bspwmrc_path=~/.config/bspwm/bspwmrc
+	sxhkdrc_path=~/.config/bspwm/sxhkdrc
 	mkdir -p ~/.config/bspwm/scripts
 	mkdir -p ~/.config/WACSBF/cache
 	mkdir -p ~/.config/bspwm/theme/wallpapers/
@@ -12,15 +13,21 @@ function makeConfig(){
 	elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
 		echo "alias wacsbf='sh \$HOME/.config/bspwm/scripts/WACSBF'" >> $zshrc_path
 	else
-	    echo "不支持的shell类型"
+	    echo "unsupported shell type"
 	    exit
 	fi
 	if [[ -f $bspwmrc_path ]]; then
 		echo "wacsbf" >> $bspwmrc_path
 	fi
+		if [[ -f $sxhkdrc_path ]]; then
+		echo "# Random wallpaper
+super + alt + w
+	wacsbf " >> $sxhkdrc_path
+	fi
+
 
 }
 makeConfig
 cp ./WACSBF $HOME/.config/bspwm/scripts/WACSBF
 
-echo "finished"
+echo "WACSBF installation finished"
