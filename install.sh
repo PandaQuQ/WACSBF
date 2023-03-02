d56836dfc1fd952f239263bf76fcd631c89c45a0
panda <mabinsoft@vip.qq.com>
2023-03-02 17:12:32 +0800

    make some change for merge this script with my dotfiles

--- install.sh
+++ install.sh
@@ -4,26 +4,23 @@ function makeConfig(){
 	bashrc_path=~/.bashrc
 	zshrc_path=~/.zshrc
 	bspwmrc_path=~/.config/bspwm/bspwmrc
-	mkdir -p ~/.config/WACSBF/scripts
+	mkdir -p ~/.config/bspwm/scripts
 	mkdir -p ~/.config/WACSBF/cache
-	mkdir -p ~/.config/WACSBF/downloaded_wallpapers
+	mkdir -p ~/.config/bspwm/theme/wallpapers/
 	if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
-		echo "PATH="\$HOME/.config/WACSBF/scripts:\$PATH"" >> $zshrc_path
-		echo "alias wacsbf='sh \$HOME/.config/WACSBF/scripts/WACSBF'" >> $zshrc_path
+		echo "alias wacsbf='sh \$HOME/.config/bspwm/scripts/WACSBF'" >> $zshrc_path
 	elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
-		echo "PATH="\$HOME/.config/WACSBF/scripts:\$PATH"" >> $zshrc_path
-		echo "alias wacsbf='sh \$HOME/.config/WACSBF/scripts/WACSBF'" >> $zshrc_path
+		echo "alias wacsbf='sh \$HOME/.config/bspwm/scripts/WACSBF'" >> $zshrc_path
 	else
 	    echo "不支持的shell类型"
 	    exit
 	fi
 	if [[ -f $bspwmrc_path ]]; then
-		echo "PATH="\$HOME/.config/WACSBF/scripts:\$PATH"" >> $bspwmrc_path
 		echo "wacsbf" >> $bspwmrc_path
 	fi
 
 }
 makeConfig
-cp ./WACSBF $HOME/.config/WACSBF/scripts/WACSBF
+cp ./WACSBF $HOME/.config/bspwm/scripts/WACSBF
 
 echo "finished"
